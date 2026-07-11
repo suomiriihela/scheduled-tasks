14aee666f6fbbfc78d31abefc2ec9d39eac4f3ba
suomiriihela <suomi.riihela@gmail.com>
2026-07-11 13:11:38 +0300

    Fix formatting of birthdays_dict comprehension

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
